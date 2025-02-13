a67394fefb8a446bfcd95d8af5c0c305923675f8
jeromelonoy8 <128391243+jeromelonoy8@users.noreply.github.com>
2025-02-13 22:12:39 +0800

    I changed the color of the body and the container. I also added functions for subtraction, multiplication, and division, changed their colors, and added a hover effect. Additionally, I changed the color of the container and the title. I also changed the color of the Clear and Exit buttons and slightly modified their shape. Furthermore, I replaced the text for addition, subtraction, division, and multiplication with their respective symbols.

--- app.js
+++ app.js
@@ -1,39 +1,59 @@
- function addnumbers()
- {
+function addnumbers(){
+    calculate("+");
+}
+function subs(){
+    calculate("-");
+}
+function multiply(){
+    calculate("*");
+}
+function division(){
+    calculate("/");
+}
+
+function calculate(operator)
+{
     const num1 = parseFloat(document.getElementById('num1').value);
     const num2 = parseFloat(document.getElementById('num2').value);
 
     if(!isNaN(num1) && !isNaN(num2))
     {
-        const result = num1 + num2;
+        let result;
+        switch(operator)
+        {
+            case "+": result = num1 + num2; break;
+            case "-": result = num1 - num2; break;
+            case "*": result = num1 * num2; break;
+            case "/": result = num2 !== 0 ? num1/num2: "Cannot divide by zero"; break;
+        }
         document.getElementById('result').value = result;
     }
     else
     {
         alert('Please enter a number');
     }
- }
+}
 function Clearbtn()
-    {
-    document.getElementById('num1').value = '';
-    document.getElementById('num2').value = '';
-    document.getElementById('result').value = '';
-    }
+   {
+   document.getElementById('num1').value = '';
+   document.getElementById('num2').value = '';
+   document.getElementById('result').value = '';
+   }
 function Exitbtn()
 {
-    window.close();
+   window.close();
 }
 document.getElementById("num1").addEventListener("keydown", function(event){
-    if (event.key == "Enter")
-        {
-            event.preventDefault();
-            addnumbers();
-        }
-    });
+   if (event.key == "Enter")
+       {
+           event.preventDefault();
+           addnumbers();
+       }
+   });
 document.getElementById("num2").addEventListener("keydown",function(event){
-    if(event.key == "Enter")
-    {
-        event.preventDefault();
-        addnumbers();
-    }
+   if(event.key == "Enter")
+   {
+       event.preventDefault();
+       addnumbers();
+   }
 });
